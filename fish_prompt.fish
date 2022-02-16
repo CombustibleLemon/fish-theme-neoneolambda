6694ba8f3cd559e4ce24336b9464b3631e9e64f9
Dan Leonard <daniel@danleonard.us>
2022-02-16 16:37:37 -0600

    Hostname over SSH to cyan

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -55,11 +55,11 @@ function fish_prompt
     if [ "$TERM" = "xterm-256color-italic" -o "$TERM" = "tmux-256color" ]
       set -g location (echo -e "\e[3mssh\e[23m")
       # set -g ssh_hostname (echo -e $blue$__fish_prompt_hostname)
-      set -g __fish_prompt_hostname $blue(hostname -f)(set_color brwhite)
+      set -g __fish_prompt_hostname $brblue(hostname -f)(set_color brwhite)
     else
       set -g location ssh
       # set -g ssh_hostname (echo -e $blue$__fish_prompt_hostname)
-      set -g __fish_prompt_hostname $blue(hostname -f)(set_color brwhite)
+      set -g __fish_prompt_hostname $brblue(hostname -f)(set_color brwhite)
     end
   end
 
